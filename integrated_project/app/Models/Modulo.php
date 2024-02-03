50f654f12fbcce116e1cdedf2e60e446e5b31b18
mariohatrabajo <121862448+mariohatrabajo@users.noreply.github.com>
2024-02-03 14:22:32 +0100

    Fix del BelongTo de Modulo y Grupo

--- integrated_project/app/Models/Modulo.php
+++ integrated_project/app/Models/Modulo.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Modulo extends Model
 {
@@ -13,5 +14,10 @@ class Modulo extends Model
     public function lecciones(): HasMany
     {
         return $this->hasMany(Leccion::class);
+    }
+
+    public function formacion(): BelongsTo
+    {
+        return $this->belongsTo(Formacion::class);
     }
 }
