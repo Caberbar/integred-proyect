6c9422849a3e7c50df7e1f46d89bb0e82fd86d64
caberbar <carlos.bernal.barrionuevo@gmail.com>
2024-02-21 13:49:09 +0100

    Add Lgin/Register button

--- integrated_project/resources/views/index.blade.php
+++ integrated_project/resources/views/index.blade.php
@@ -11,13 +11,6 @@
             <div class="page-block">
                 <div class="row align-items-center">
                     <div class="col-md-12">
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="../navigation/index.html">Home</a></li>
-                            <li class="breadcrumb-item"><a href="javascript: void(0)">Dashboard</a></li>
-                            <li class="breadcrumb-item" aria-current="page">Home</li>
-                        </ul>
-                    </div>
-                    <div class="col-md-12">
                         <div class="page-header-title">
                             <h2 class="mb-0">Home</h2>
                         </div>
