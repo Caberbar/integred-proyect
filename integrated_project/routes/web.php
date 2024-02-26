0c0d60a42a0652b3c1cfe07e430ba06b2e167f04
aleks <aprokhorenkodev@gmail.com>
2024-02-26 20:54:55 +0100

    Elimanda carpeta mylogin, elimandas las rutas antiguas del web.php, eliminado controladores de estas y sus request

--- integrated_project/routes/web.php
+++ integrated_project/routes/web.php
@@ -20,46 +20,6 @@ use App\Http\Controllers\UserController;
  */
 Route::view('/', 'index')->name('home');
 
-/**
- *  --------------------------------
- *      RUTAS DE LOS PROFESORES
- *  --------------------------------
- */
-Route::view('/profesor', 'profesor')->name('profesor');
-Route::get('/profesor/create', [ProfesorController::class, 'create'])->name('profesor.create');
-Route::post('/profesor/insert', [ProfesorController::class, 'insert'])->name('profesor.insert');
-/**
- *  --------------------------------
- *      RUTAS DE LAS LECCIONES
- *  --------------------------------
- */
-Route::view('/lecciones', 'lecciones')->name('lecciones');
-Route::get('/lecciones/create', [LeccionController::class, 'create'])->name('lecciones.create');
-Route::post('/lecciones/insert', [LeccionController::class, 'insert'])->name('lecciones.insert');
-/**
- *  --------------------------------
- *      RUTAS DE LOS MODULOS
- *  --------------------------------
- */
-Route::view('/modulos', 'modulos')->name('modulos');
-Route::get('/modulos/create', [ModuloController::class, 'create'])->name('modulos.create');
-Route::post('/modulos/insert', [ModuloController::class, 'insert'])->name('modulos.insert');
-/**
- *  --------------------------------
- *      RUTAS DE LOS FORMACIÓN
- *  --------------------------------
- */
-Route::view('/formaciones', 'formaciones')->name('formaciones');
-Route::get('/formaciones/create', [FormacionController::class, 'create'])->name('formaciones.create');
-Route::post('/formaciones/insert', [FormacionController::class, 'insert'])->name('formaciones.insert');
-/**
- *  --------------------------------
- *      RUTAS DE LOS GRUPOS
- *  --------------------------------
- */
-Route::view('/grupos', 'grupos')->name('grupos');
-Route::get('/grupos/create', [GrupoController::class, 'create'])->name('grupos.create');
-Route::post('/grupos/insert', [GrupoController::class, 'insert'])->name('grupos.insert');
 
 /**
  *  --------------------------------
