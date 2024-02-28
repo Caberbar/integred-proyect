8051dfadb6fe3e5ce102e6a0875dd0eb5192430a
aleks <aprokhorenkodev@gmail.com>
2024-02-28 18:48:59 +0100

    Edit Rol table user

--- integrated_project/app/Livewire/RolTable.php
+++ integrated_project/app/Livewire/RolTable.php
@@ -3,16 +3,55 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use App\Models\Profesor;
+use App\Models\User;
 use App\Models\Role;
 
 class RolTable extends Component
 {
 
-    public $accion = 'edit';
+    public $accion = 'Edit';
+    public $nombre_usuario, $id_usuario,$rol;
+    public User $usuario;
+
+    public function mount(){
+        $this->nombre_usuario = null;
+        $this->rol = null;
+        $this->usuario = new User;
+    }
+
     public function render()
     {
-
-        return view('livewire.rol-table');
+        $usuarios = User::all();
+        $roles = Role::all();
+        return view('livewire.rol-table', compact('usuarios', 'roles'));
     }
+
+    public function modal($usuario_id){
+        $this->usuario = User::findOrFail($usuario_id);
+        $this->nombre_usuario = $this->usuario->name;
+        $this->id_usuario = $this->usuario->id;
+    }
+
+    public function save(){
+        $this->validate();
+
+        $this->usuario->roles()->attach($this->rol);
+
+        $this->mount();
+        $this->dispatch('cerrar_modal');
+    }
+
+
+    protected $rules = [
+        'id_usuario' =>[
+            'required',
+            'integer',
+            'min:0'
+        ],
+        'rol'=>[
+            'required',
+            'integer',
+            'min:0'
+        ]
+    ];
 }
--- integrated_project/resources/views/livewire/rol-table.blade.php
+++ integrated_project/resources/views/livewire/rol-table.blade.php
@@ -1,23 +1,35 @@
 <div>
     <div>
-        <!-- Modal -->
-        <div class="modal fade" id="RolloModal" tabindex="-1" aria-labelledby="RolModalLabel" aria-hidden="true" wire:ignore.self>
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="RolModalLabel"> {{$accion}} Rol</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"  id='cerrar_modal'></button>
-                    </div>
-                    <div class="modal-body">
+    <!-- Modal -->
+    <div class="modal fade" id="RolModal" tabindex="-1" aria-labelledby="RolModalLabel" aria-hidden="true" wire:ignore.self>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="RolModalLabel"> {{ $accion }} Rol</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="">The name of user:</label>
+                    <p>{{$nombre_usuario}}</p>
 
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id='cerrar_modal'>CLOSE</button>
-                        <button type="button" class="btn btn-primary" wire:click='save' wire:loading.attr='disable' wire:target='save'> {{ $accion }} Changes</button>
-                    </div>
+                    @error('rol')
+                        <p>El rol escogido esta mal garrulo</p>
+                    @enderror
+                    <select wire:model="rol">
+                        <option value="null">Select any rol</option>
+                        @foreach ($roles as $rol)
+                            <option value="{{$rol->id}}">{{$rol->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <br><br>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id='cerrar_modal'>Close</button>
+                    <button type="button" class="btn btn-primary" id="insert-submit" wire:click='save' wire:loading.attr='disable' wire:target='save' > {{ $accion }}</button>
                 </div>
             </div>
         </div>
+    </div>
         <div class="row">
             <!-- DOM/Jquery table start -->
             <div class="col-sm-12">
@@ -45,11 +57,7 @@
                                 <br>
                                 <div class="row">
                                     <div class="col-sm-12 col-md-12">
-                                        <!-- BOTON VENTANA MODAL -->
-                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                            data-bs-target="#RolModal" wire:click='modal()'>
-                                            Create teacher
-                                        </button>
+
                                     </div>
                                 </div>
                             </div>
@@ -57,67 +65,38 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive dt-responsive">
-                        @if ($modulos->isNotEmpty())
-                            <table id="dom-jqry" class="table table-striped table-bordered nowrap">
+                            <table>
                                 <thead>
                                     <tr>
-                                        <th wire:click="doSort('denominacion')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="denominacion" columnName="Denomination" /></th>
-                                        <th wire:click="doSort('especialidad')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="especialidad" columnName="Speciality" /></th>
-                                        <th wire:click="doSort('siglas')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="siglas" columnName="Acronym" /></th>
-                                        <th wire:click="doSort('curso')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="curso" columnName="Cours" /></th>
-                                        <th wire:click="doSort('horas')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="horas" columnName="Hours" /></th>
-                                        <th wire:click="doSort('formacion_siglas')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="formacion_siglas" columnName="Acronym" /></th>
-                                        <th>Actions</th>
+                                        <th>Usuario</th>
+                                        <th>Rol</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($modulos as $modulo)
-                                    <tr>
-                                        <td>{{ $modulo->denominacion }}</td>
-                                        <td>{{ $modulo->especialidad }}</td>
-                                        <td>{{ $modulo->siglas }}</td>
-                                        <td>{{ $modulo->curso }}</td>
-                                        <td>{{ $modulo->horas }}</td>
-                                        <td>{{ $modulo->formacion->siglas}}</td>
-                                        <td>
-                                            <ul class="list-inline me-auto mb-0">
-                                                <li class="list-inline-item align-bottom" data-bs-toggle="tooltip" aria-label="Edit" data-bs-original-title="Edit">
-                                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#RolModal" wire:click="modal({{ $modulo->id }})">
-                                                        EDIT
-                                                    </button>
-                                                </li>
-                                                <li class="list-inline-item align-bottom" data-bs-toggle="tooltip" aria-label="Delete" data-bs-original-title="Delete">
-                                                    <button class="btn btn-primary" wire:click="delete({{ $modulo->id }})" wire:loading.attr='disable' wire:target='delete'>
-                                                        DELETE
-                                                    </button>
-                                                </li>
-                                            </ul>
-                                        </td>
-                                    </tr>
-                                    @endforeach
+                                    @forelse ($usuarios as $usuario)
+                                        <tr>
+                                            <td>{{ $usuario->name }}</td>
+                                            <?php
+                                                $roles = $usuario->roles;
+                                                $rol;
+                                                if($roles->isEmpty()){
+                                                    $rol = 'They don´t have any rol';
+                                                }else{
+                                                    $rol = $roles->pluck('name')->implode(', ');
+                                                }
+                                            ?>
+                                            <td><?php echo $rol ?></td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#RolModal" wire:click='modal({{$usuario->id}})'>
+                                                    EDIT
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @empty
+
+                                    @endforelse
                                 </tbody>
                             </table>
-                            @elseif($modulos->isEmpty() && $search != '')
-                            <table id="dom-jqry" class="table table-striped table-bordered nowrap">
-                                <thead>
-                                    <tr>
-                                        <th wire:click="doSort('usu_seneca')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="usu_seneca" columnName="Seneca User" /></th>
-                                        <th wire:click="doSort('nombre')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="nombre" columnName="Name" /></th>
-                                        <th wire:click="doSort('apellido1')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="apellido1" columnName="First Name" /></th>
-                                        <th wire:click="doSort('apellido2')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="apellido2" columnName="Last Name" /></th>
-                                        <th wire:click="doSort('especialidad')" class="column-tables"><x-datatable-item :sortColumn="$sortColumn" :sortDirection="$sortDirection" columnNameVar="especialidad" columnName="Speciality" /></th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td colspan="6">No results found.</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            @else
-                            <p>No modules found.</p>
-                            @endif
                         </div>
                     </div>
                     <div class="card-header">
@@ -125,7 +104,7 @@
                             <div id="dom-jqry_wrapper" class="dataTables_wrapper dt-bootstrap5">
                                 <div class="row pagination-center">
                                     <div class="col-sm-12 col-md-11">
-                                        {{$modulos->links()}}
+                                        {{-- {{$modulos->links()}} --}}
                                     </div>
                                 </div>
                             </div>
